cf664483c4349c44f290c9b0e01dcf3c86714bbd
Andrew Pickett <andrew.j.pickett1@gmail.com>
2017-03-27 17:57:15 -0700

    no message

--- SwiftApp/ViewController.swift
+++ SwiftApp/ViewController.swift
@@ -5,14 +5,37 @@
 //  Created by Andrew Pickett on 3/27/17.
 //  Copyright © 2017 Andrew Pickett. All rights reserved.
 //
+// new comment to test Git
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    var buttonCount = 0
+    
+    @IBAction func button_tapped(_ sender: Any) {
+    coolLabel.text = "Hello There!"
+    buttonCount+=1
+        if buttonCount >= 10{
+            coolLabel.text = "You tapped the button 10 times"
+        }
+        // onlt use print to debug 
+    
+    }
+    
+    @IBAction func different_button(_ sender: Any) {
+    coolLabel.text = "Buttons are cool"
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        coolLabel.text = "Hello There"
+        // dont need () at the end of self statement for some reason
     }
 
     override func didReceiveMemoryWarning() {
